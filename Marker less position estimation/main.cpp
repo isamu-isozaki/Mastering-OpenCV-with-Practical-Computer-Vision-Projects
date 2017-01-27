e21a131e489714af1d36133d31fcf204b6acf3df
Isamu Isozaki <isamu.website@gmail.com>
2017-01-28 08:06:24 +0900

    Some problems yet functional

--- Marker less position estimation/main.cpp
+++ Marker less position estimation/main.cpp
@@ -14,11 +14,24 @@ int main(int argc, char** argv) {
 	CameraCalibration calibration;
 	cv::Mat markerPose;
 
-	std::vector<std::string> fileList;
 
-	calibrator.findPoints_caliberate(fileList, calibration);
-	
-	patternDetector.findPattern("pattern.png", "dstImg.jpg", info, true, true);
-	patternDetector.estimatePosition(patternDetector.m_pattern, calibration, markerPose, true);
+	std::vector<std::string> fileList;
+	for (int i = 0; i < 20; i++){
+		fileList.push_back("./calibrationImg/dst" + std::to_string(i) + ".jpg");
+	}//get the images for the calibration
+
+	calibrator.findPoints_caliberate(fileList, calibration);//get calibration params
+
+	std::cout << "Intrinsic Matrix" << calibration.m_intrinsic << std::endl;
+	std::cout << "Distortion Vector" << calibration.m_distortion << std::endl;
+
+
+
+	patternDetector.findPattern("pattern.png", "dst.jpg", info, true, true);//detect the American flag in dst.jpg
+	patternDetector.estimatePosition(patternDetector.m_pattern, calibration, markerPose, true);//get the position of dst.jpg
+
+	std::cout << "homography matrix" << patternDetector.info.homography << std::endl;//currently disfunctional
+	std::cout << "extrinsic matrix" << patternDetector.m_extrinsic << std::endl;
+	std::cout << "marker pose" << patternDetector.m_markerPose << std::endl;//inverse of extrinsic matrix
 	//marker pose now has the marker position
 }
